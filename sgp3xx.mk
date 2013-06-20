9687ec3a1c6f0960c7e37b076e36e4068253998a
nycbjr <nycbjr@gmail.com>
2013-06-20 12:04:32 -0400

    gps

--- sgp3xx.mk
+++ sgp3xx.mk
@@ -62,6 +62,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
+    $(COMMON_PATH)/rootdir/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
     $(COMMON_PATH)/rootdir/init.target.rc:root/init.target.rc \
     $(COMMON_PATH)/rootdir/fstab.qcom:recovery/root/fstab.qcom \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh
