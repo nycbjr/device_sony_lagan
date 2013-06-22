54fef23ea168b922e75a9eac0d62ceafcc23cd30
nycbjr <nycbjr@gmail.com>
2013-06-22 17:36:12 -0400

    trascing boot process

--- sgp3xx.mk
+++ sgp3xx.mk
@@ -169,6 +169,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 # Custom init / uevent
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
+    $(COMMON_PATH)/rootdir/init.rc:root/init.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(COMMON_PATH)/rootdir/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
